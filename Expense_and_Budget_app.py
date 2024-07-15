2f928796716a9ce7f88559b4bcd9a3b91f6819cb
Ramy Yousif <ramy_yousif@hotmail.fr>
2024-07-15 23:55:00 +0100

    Update Expense_and_Budget_app.py

--- Expense_and_Budget_app.py
+++ Expense_and_Budget_app.py
@@ -1,12 +1,6 @@
 # Expense and Budget app.
 
-# I created this program so that a user can:
-# - View, add, update and delete all and any expenses they make,
-# - View, add, update and delete all and any income they gain,
-# - View, update, delete and set a budget on any category,
-# - View, update, delete, set financial goals and view their progress towards said financial goal
-# depending on their total income and expenses.
-# This data is then stored and updated on a database using SQL.
+
 
 
 # Importing sqlite3.
